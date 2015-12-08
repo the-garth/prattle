e5945637245c638592fa362c08d4aba4f59dc8c5
amogower <amogower@gmail.com>
2015-12-08 08:02:22 +0000

    A little more code cleanup

--- test/controllers/conversations_controller_test.rb
+++ test/controllers/conversations_controller_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class ConversationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
--- test/controllers/uploads_controller_test.rb
+++ test/controllers/uploads_controller_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class UploadsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 end
